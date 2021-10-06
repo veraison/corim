e0109bfa2522edc8799541243317e90ac1e4cfb2
Thomas Fossati <thomas.fossati@arm.com>
2021-10-06 16:31:22 +0100

    CLI, "corim verify" subcommand

---
+++ cli/cmd/corimVerify_test.go
@@ -0,0 +1,133 @@
+// Copyright 2021 Contributors to the Veraison project.
+// SPDX-License-Identifier: Apache-2.0
+
+package cmd
+
+import (
+	"testing"
+
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_CorimVerifyCmd_unknown_argument(t *testing.T) {
+	cmd := NewCorimVerifyCmd()
+
+	args := []string{"--unknown-argument=val"}
+	cmd.SetArgs(args)
+
+	err := cmd.Execute()
+	assert.EqualError(t, err, "unknown flag: --unknown-argument")
+}
+
+func Test_CorimVerifyCmd_mandatory_args_missing_corim_file(t *testing.T) {
+	cmd := NewCorimVerifyCmd()
+
+	args := []string{
+		"--key-file=ignored.jwk",
+	}
+	cmd.SetArgs(args)
+
+	err := cmd.Execute()
+	assert.EqualError(t, err, "no CoRIM supplied")
+}
+
+func Test_CorimVerifyCmd_mandatory_args_missing_key_file(t *testing.T) {
+	cmd := NewCorimVerifyCmd()
+
+	args := []string{
+		"--corim-file=ignored.jwk",
+	}
+	cmd.SetArgs(args)
+
+	err := cmd.Execute()
+	assert.EqualError(t, err, "no key supplied")
+}
+
+func Test_CorimVerifyCmd_non_existent_signed_corim_file(t *testing.T) {
+	cmd := NewCorimVerifyCmd()
+
+	args := []string{
+		"--corim-file=nonexistent.cbor",
+		"--key-file=ignored.jwk",
+	}
+	cmd.SetArgs(args)
+
+	fs = afero.NewMemMapFs()
+
+	err := cmd.Execute()
+	assert.EqualError(t, err, "error loading signed CoRIM from nonexistent.cbor: open nonexistent.cbor: file does not exist")
+}
+
+func Test_CorimVerifyCmd_bad_signed_corim(t *testing.T) {
+	cmd := NewCorimVerifyCmd()
+
+	args := []string{
+		"--corim-file=bad.txt",
+		"--key-file=ignored.jwk",
+	}
+	cmd.SetArgs(args)
+
+	fs = afero.NewMemMapFs()
+	err := afero.WriteFile(fs, "bad.txt", []byte("hello!"), 0644)
+	require.NoError(t, err)
+
+	err = cmd.Execute()
+	assert.EqualError(t, err, "error decoding signed CoRIM from bad.txt: failed CBOR decoding for COSE-Sign1 signed CoRIM: unexpected EOF")
+}
+
+func Test_CorimVerifyCmd_non_existent_key_file(t *testing.T) {
+	cmd := NewCorimVerifyCmd()
+
+	args := []string{
+		"--corim-file=ok.cbor",
+		"--key-file=nonexistent.jwk",
+	}
+	cmd.SetArgs(args)
+
+	fs = afero.NewMemMapFs()
+	err := afero.WriteFile(fs, "ok.cbor", testSignedCorimValid, 0644)
+	require.NoError(t, err)
+
+	err = cmd.Execute()
+	assert.EqualError(t, err, "error loading verifying key from nonexistent.jwk: open nonexistent.jwk: file does not exist")
+}
+
+func Test_CorimVerifyCmd_invalid_key_file(t *testing.T) {
+	cmd := NewCorimVerifyCmd()
+
+	args := []string{
+		"--corim-file=ok.cbor",
+		"--key-file=invalid.jwk",
+	}
+	cmd.SetArgs(args)
+
+	fs = afero.NewMemMapFs()
+	err := afero.WriteFile(fs, "ok.cbor", testSignedCorimValid, 0644)
+	require.NoError(t, err)
+	err = afero.WriteFile(fs, "invalid.jwk", []byte("{}"), 0644)
+	require.NoError(t, err)
+
+	err = cmd.Execute()
+	assert.EqualError(t, err, "error loading verifying key from invalid.jwk: failed to unmarshal JWK set: failed to unmarshal key from JSON headers: invalid key type from JSON ()")
+}
+
+func Test_CorimVerifyCmd_ok(t *testing.T) {
+	cmd := NewCorimVerifyCmd()
+
+	args := []string{
+		"--corim-file=ok.cbor",
+		"--key-file=ok.jwk",
+	}
+	cmd.SetArgs(args)
+
+	fs = afero.NewMemMapFs()
+	err := afero.WriteFile(fs, "ok.cbor", testSignedCorimValid, 0644)
+	require.NoError(t, err)
+	err = afero.WriteFile(fs, "ok.jwk", testECKey, 0644)
+	require.NoError(t, err)
+
+	err = cmd.Execute()
+	assert.NoError(t, err)
+}
