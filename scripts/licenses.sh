de149e9b7682941d0c35ee34766161943645fbd5
Jagannathan Raman <jag.raman@oracle.com>
2024-08-22 15:33:58 -0400

    cocli: move cocli to a standalone repo
    Relocate cocli to a separate repo under Veraison Organization. Preserve
    git history during the process.
    
    cocli will be available at https://github.com/veraison/cocli
    
    Signed-off-by: Jagannathan Raman <jag.raman@oracle.com>

--- scripts/licenses.sh
+++ scripts/licenses.sh
@@ -8,7 +8,6 @@ type go-licenses &> /dev/null || go get github.com/google/go-licenses
 
 MODULES+=("github.com/veraison/corim/corim")
 MODULES+=("github.com/veraison/corim/comid")
-MODULES+=("github.com/veraison/corim/cocli")
 
 for module in ${MODULES[@]}
 do
