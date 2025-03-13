f2e3d7a1749a1af5d58b3a534b3f42a3e256d7e3
Yogesh Deshpande <yogesh.deshpande@arm.com>
2025-03-13 18:29:20 +0000

    [WIP] INtel Expression Changes
    Signed-off-by: Yogesh Deshpande <yogesh.deshpande@arm.com>

--- comid/tdx-profile/common.go
+++ comid/tdx-profile/common.go
@@ -3,7 +3,33 @@
 
 package tdx
 
+type numOperator uint
+
+type taggednumOperator numOperator
+
+const (
+	gt = iota + 1
+	ge
+	lt
+	le
+)
+
 func isType[T any](v any) bool {
 	_, ok := v.(T)
 	return ok
 }
+
+var (
+	stringTonumOperator = map[string]numOperator{
+		"gt": gt,
+		"ge": ge,
+		"lt": lt,
+		"le": le,
+	}
+	numOperatorToString = map[numOperator]string{
+		gt: "gt",
+		ge: "ge",
+		lt: "lt",
+		le: "le",
+	}
+)
