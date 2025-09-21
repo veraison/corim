533c7cf0bedbb3e576783ebf7963debbeffb9f8b
Shivam Kumar <shivam.kumarsingh2324@gmail.com>
2025-09-22 03:28:32 +0530

    fixed  allow indefLength across cbor.go files
    Signed-off-by: Shivam Kumar <shivam.kumarsingh2324@gmail.com>

--- corim/cbor.go
+++ corim/cbor.go
@@ -45,7 +45,7 @@ func corimTags() cbor.TagSet {
 
 func initCBOREncMode() (en cbor.EncMode, err error) {
 	encOpt := cbor.EncOptions{
-		IndefLength: cbor.IndefLengthForbidden,
+		IndefLength: cbor.IndefLengthAllowed,
 		TimeTag:     cbor.EncTagRequired,
 	}
 	return encOpt.EncModeWithTags(corimTags())
@@ -53,7 +53,7 @@ func initCBOREncMode() (en cbor.EncMode, err error) {
 
 func initCBORDecMode() (dm cbor.DecMode, err error) {
 	decOpt := cbor.DecOptions{
-		IndefLength: cbor.IndefLengthForbidden,
+		IndefLength: cbor.IndefLengthAllowed,
 		TimeTag:     cbor.DecTagRequired,
 	}
 	return decOpt.DecModeWithTags(corimTags())
