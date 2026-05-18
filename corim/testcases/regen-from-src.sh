edc5c968bbfdbc64a0365561b969746e9a8e41a8
Sergei Trofimov <sergei.trofimov@arm.com>
2026-05-18 14:58:05 +0100

    test: unmarshal all diag examples from the spec
    Add tests to unmarshal compiled diag examples from the spec GitHub
    repository[1]. Specifically, all corim-*.diag and comid-*.diag are added
    as test case sources in the corim and comid sub-packages respectively,
    and a test added to each that runs through all compiled examples and
    ensures that they unmarshal without error.
    
    [1]: https://github.com/ietf-rats-wg/draft-ietf-rats-corim/tree/draft-ietf-rats-corim-10/cddl/examples
    
    Signed-off-by: Sergei Trofimov <sergei.trofimov@arm.com>

--- corim/testcases/regen-from-src.sh
+++ corim/testcases/regen-from-src.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/bash
-# Copyright 2024 Contributors to the Veraison project.
+# Copyright 2024-2026 Contributors to the Veraison project.
 # SPDX-License-Identifier: Apache-2.0
 set -e
 
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 GEN_TESTCASE=$(go env GOPATH)/bin/gen-testcase
+
+if [[ "$(type -p diag2cbor.rb)" == "" ]]; then
+	echo "ERROR: please install ruby-cbor-diag package"
+	exit 1
+fi
 
 if [[ ! -f ${GEN_TESTCASE} ]]; then
 	echo "installing gen-testcase"
@@ -25,6 +32,14 @@ for case in "${testcases[@]}"; do
 
 	${GEN_TESTCASE} -s src/ec-p256.jwk -m src/meta.yaml "src/${case}.yaml" \
 		-o "signed-${case}.cbor"
+done
+
+for case in "$THIS_DIR"/src/*.diag; do
+	outfile=$(basename "${case%%.diag}").cbor
+
+	echo "generating $outfile"
+
+	diag2cbor.rb "$case" > "$THIS_DIR/$outfile"
 done
 
 echo "done."
